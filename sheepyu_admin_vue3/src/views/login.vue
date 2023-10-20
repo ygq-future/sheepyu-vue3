454a64962978e1945217bd8626b95f9d493b6f81
ygq <3029944576@qq.com>
2023-10-20 16:57:01 +0800

    优化登录页面代码

--- sheepyu_admin_vue3/src/views/login.vue
+++ sheepyu_admin_vue3/src/views/login.vue
@@ -1,41 +1,53 @@
 <template>
-  <div style="position: fixed; top: 0; left: 0; bottom: 0; right: 0">
-    <canvas id="canvas" />
+  <div style='position: fixed; top: 0; left: 0; bottom: 0; right: 0'>
+    <canvas id='canvas' />
   </div>
-  <div class="theme-select">
-    <el-radio-group v-model="config.layout.colorModeIndex" @change="onColorModeChange" size="small">
-      <el-radio-button v-for="(item, index) in config.layout.colorMode" :label="index">
+  <div class='theme-select'>
+    <el-radio-group v-model='config.layout.colorModeIndex' @change='onColorModeChange' size='small'>
+      <el-radio-button v-for='(item, index) in config.layout.colorMode' :label='index'>
         {{ item }}
       </el-radio-button>
     </el-radio-group>
   </div>
-  <div class="box full">
-    <img class="img" src="@/assets/logo.png" alt="" />
-    <span class="title">SY管理后台登录</span>
-    <el-form ref="formRef" :model="form" :rules="rules" label-width="0" size="default">
-      <el-form-item prop="login">
-        <el-input autofocus prefix-icon="el-icon-User" v-model="form.login" placeholder="用户名/邮箱/手机号" />
-      </el-form-item>
-      <el-form-item prop="password">
-        <el-input prefix-icon="el-icon-Lock" type="password" v-model="form.password" placeholder="密码" />
-      </el-form-item>
-      <el-form-item class="code" prop="code">
+  <div class='box full'>
+    <img class='img' src='@/assets/logo.png' alt='' />
+    <span class='title'>SY管理后台登录</span>
+    <el-form ref='formRef' :model='form' :rules='rules' label-width='0' size='default'>
+      <el-form-item prop='login'>
         <el-input
-          :disabled="!captchaEnable"
-          prefix-icon="el-icon-More"
-          v-model="form.code"
-          placeholder="验证码"
-          @keydown.enter="submit(formRef)"
+          autofocus
+          prefix-icon='el-icon-User'
+          v-model='form.login'
+          placeholder='用户名/邮箱/手机号'
+          @keydown.enter.native='submit'
         />
-        <img v-if="captchaEnable" :src="captchaInfo.base64" alt="验证码" @click="getCaptcha" title="点击刷新" />
-        <span v-else style="color: lightgray; user-select: none">不用验证码</span>
+      </el-form-item>
+      <el-form-item prop='password'>
+        <el-input
+          prefix-icon='el-icon-Lock'
+          type='password'
+          v-model='form.password'
+          placeholder='密码'
+          @keydown.enter.native='submit'
+        />
+      </el-form-item>
+      <el-form-item class='code' prop='code'>
+        <el-input
+          :disabled='!captchaEnable'
+          prefix-icon='el-icon-More'
+          v-model='form.code'
+          placeholder='验证码'
+          @keydown.enter.native='submit'
+        />
+        <img v-if='captchaEnable' :src='captchaInfo.base64' alt='验证码' @click='getCaptcha' title='点击刷新' />
+        <span v-else style='color: lightgray; user-select: none'>不用验证码</span>
       </el-form-item>
     </el-form>
-    <el-button type="primary" @click="submit(formRef)">登录</el-button>
+    <el-button type='primary' @click='submit'>登录</el-button>
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup lang='ts'>
 import { useConfig } from '@/stores/config/config'
 import { useUser } from '@/stores/user/user'
 import { useRouter } from 'vue-router'
@@ -68,28 +80,24 @@ const rules: FormRules = {
   password: [{ required: true, message: '密码不能为空', trigger: 'blur' }]
 }
 
-function submit(formRef?: InstanceType<typeof ElForm>) {
-  formRef?.validate(isValid => {
-    if (!isValid) {
-      return
-    }
-
-    let data = { ...toRaw(form) }
-    form.code = ''
-    data.key = captchaInfo.key
-    data.code = captchaInfo.arithmetic + data.code
-    loginApi(data).then(res => {
-        user.setAuthInfo(res.data)
-        userInfoApi().then(res => {
-          user.setUserInfo(res.data)
-          loadDict()
-          const redirectUrl = route.query.redirectUrl as string
-          router.replace(redirectUrl ? redirectUrl : '/')
-        })
-      }).catch(() => {
-        getCaptcha()
-      })
-  })
+async function submit() {
+  const res = await formRef.value?.validate(valid => valid)
+  if (!res) return
+  let data = { ...toRaw(form) }
+  form.code = ''
+  data.key = captchaInfo.key
+  data.code = captchaInfo.arithmetic + data.code
+  try {
+    const { data: loginUser } = await loginApi(data)
+    user.setAuthInfo(loginUser)
+    const { data: userInfo } = await userInfoApi()
+    user.setUserInfo(userInfo)
+    loadDict()
+    const redirectUrl = route.query.redirectUrl as string
+    await router.replace(redirectUrl ? redirectUrl : '/')
+  } catch (err) {
+    await getCaptcha()
+  }
 }
 
 async function getCaptcha() {
@@ -129,7 +137,7 @@ onMounted(async () => {
 })
 </script>
 
-<style scoped lang="scss">
+<style scoped lang='scss'>
 .img {
   position: absolute;
   top: -40px;
